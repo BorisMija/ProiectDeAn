42c20d719fba1178998ce52e80fb127210a8bc50
BorisMija <boris.mija@isa.utm.md>
2025-05-23 13:58:36 +0300

    noua pagina

---
+++ CryptoWallet/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace CryptoWallet.Controllers
+{
+    public class BlogController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Blog()
+        {
+            // Poți trimite date către view aici, dacă vrei
+            return View();
+        }
+    }
+}
