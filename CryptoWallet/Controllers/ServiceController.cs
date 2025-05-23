a63882c406143e767016a0f3dd53ad3cef080abd
BorisMija <boris.mija@isa.utm.md>
2025-05-23 15:24:21 +0300

    new pages nou

---
+++ CryptoWallet/Controllers/ServiceController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace CryptoWallet.Controllers
+{
+    public class ServiceController : Controller
+    {
+        // GET: /Service/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
