5a9d22613f2592f3a6bc4f66ae4ff834ed412e11
George <64400030+YamatoNM@users.noreply.github.com>
2025-05-27 20:56:53 +0300

    LogIn and Register appearance changed

---
+++ CryptoWallet/Models/UserData/UserData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CryptoWallet.Models.UserData
+{
+	public class UserData
+	{
+          public string Username { get; set; }
+          public string Email { get; set; }
+          public int Level { get; set; }
+          public bool IsBlocked { get; set; }
+          public decimal AccountBalance { get; set; }
+          public DateTime LastLogin { get; set; }
+
+          // Additional properties can be added as needed
+          // e.g., ProfilePictureUrl, LastLoginDate, etc.
+     }
+}
